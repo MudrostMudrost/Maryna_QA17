b9929a060cd10fa9988c8ab4c345027ab8916f3a
Maryna <Shabanova111@gmail.com>
2019-01-21 20:30:19 +0200

    Ebay

--- first-Silenium-Project/src/test/java/OpenChromeBrowser.java
+++ first-Silenium-Project/src/test/java/OpenChromeBrowser.java
@@ -1,8 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
@@ -12,11 +15,14 @@ public class OpenChromeBrowser {
     @BeforeClass
     public void setUp(){
         wd=new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         }
 
         @Test
     public void chromeBrowserTest(){
-        wd.get("http:/google.com");
+        wd.get("http://google.com");
+        wd.findElement(By.name("q")).click();
+        
         }
 
         @AfterClass
---
+++ first-Silenium-Project/src/test/java/OpenFirefox.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class OpenFirefox{
+    WebDriver wd;
+    @BeforeMethod
+    public void setUp(){
+        wd=new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+    }
+
+
+    @Test
+    public void testBaySearch(){
+        wd.get("https://www.ebay.com/");
+        wd.findElement(By.id("gh-ac")).click();
+        wd.findElement(By.id("gh-ac")).clear();
+        wd.findElement(By.id("gh-ac")).sendKeys("glasses");
+        wd.findElement(By.id("gh-btn")).click();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+       // wd.quit();
+    }
+}
